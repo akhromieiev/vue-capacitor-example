bf1f5d70d70c865d7914b2a25e8e553d3fb25676
ARoman <qpbd@ukr.net>
2018-05-11 12:35:45 +0300

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# vue-capacitor-example
+
+> A Vue.js project with Capacitor
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# install Capacitor 
+npm install --save @capacitor/core @capacitor/cli
+
+# init Capacitor
+npx cap init vue-capacitor-example com.roma.vueCapacitorExample
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <button v-on:click="getDeviceInfo()">Get Device Data</button>
+    <p> {{ info }}</p>
+  </div>
+</template>
+
+<script>
+import { Plugins } from "@capacitor/core";
+const { Device } = Plugins;
+
+export default {
+  name: "App",
+  data() {
+    return {
+      info: null
+    };
+  },
+  methods: {
+    async getDeviceInfo() {
+      this.info = await Device.getInfo();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
